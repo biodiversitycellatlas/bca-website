0385f7a6cfa68d2cd04112254db6f3677330dbcc
Nuno Agostinho <nuno.agostinho@crg.eu>
2024-11-20 16:06:33 +0100

    Add licenses to About section

---
+++ web_app/templatetags/custom_tags.py
@@ -0,0 +1,17 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+from datetime import datetime
+import os
+
+register = template.Library()
+
+@register.simple_tag
+def file_last_modified(file_path, format_string="%d %B %Y"):
+    """
+    Returns the last modified date of a file.
+    """
+    try:
+        timestamp = os.path.getmtime(file_path)
+        return datetime.fromtimestamp(timestamp).strftime(format_string)
+    except FileNotFoundError:
+        return None
