336ea1202f091fb34af079ed788ddbdebce30a0e
Nuno Agostinho <nuno.agostinho@crg.eu>
2024-12-16 11:46:17 +0100

    Update atlas overview to use REST API

--- web_app/static/web_app/js/heatmap.js
+++ web_app/static/web_app/js/heatmap.js
@@ -1,26 +1,35 @@
-function createExpressionHeatmap(id, expr, genes, metacells) {
-	console.log(expr);
+function createExpressionHeatmap(id, species, url, n_markers, fc_min) {
+	var params = new URLSearchParams({
+		species: species,
+		fc_min: fc_min,
+		n_markers: n_markers,
+		sort_genes: true,
+		log2: true,
+		clip_log2: 6,
+		limit: 0
+	});
+	var apiURL = url + "?" + params.toString();
+
     var chart = {
 		"$schema": "https://vega.github.io/schema/vega-lite/v5.json",
 		"title": {
-    		"text": genes + ' genes, ' + metacells + ' metacells',
+    		// "text": genes + ' genes, ' + metacells + ' metacells',
     		"fontWeight": "normal",
     		"anchor": "start"
   		},
-		"datasets": { "expr_data": expr },
 		"height": "container",
-		"transform": [{"calculate": "toNumber(datum.metacell__name)", "as": "metacell__name"}],
-		"data": {"name": "expr_data"},
+		"transform": [{"calculate": "toNumber(datum.metacell_name)", "as": "metacell_name"}],
+		"data": {"name": "exprData", "url": apiURL },
 		"vconcat": [{
 		  	"width": "container",
 		  	"mark": "rect",
 	      	"encoding": {
-	        	"x": {"field": "metacell__name", "axis": {"labels": false, "ticks": false}},
+	        	"x": {"field": "metacell_name", "axis": {"labels": false, "ticks": false}},
 	        	"color": {
-	        		"field": "metacell__color", "legend": false,
-	        		"scale": {"range": {"field": "metacell__color"}}
-	        	},
-	        	"tooltip": [ {"field": "metacell__name"}, {"field": "metacell__type"} ]
+	        		"field": "metacell_color", "legend": false,
+	    			"scale": {"range": {"field": "metacell_color"}}
+	    		},
+	        	"tooltip": [ {"field": "metacell_name"}, {"field": "metacell_type"}, {"field": "metacell_color"} ]
 	      	}
 	    }, {
 	    	"width": "container",
@@ -28,36 +37,37 @@ function createExpressionHeatmap(id, expr, genes, metacells) {
 			"mark": {"type": "rect", "tooltip": {"content": "data"}},
 			"encoding": {
 	    		"x": {
-	    			"field": "metacell__name",
+	    			"field": "metacell_name",
 	    			"axis": { "labels": false, "ticks": false }
 	    		},
 	    		"y": {
-	    			"field": "gene__name",
+	    			"field": "gene_name",
 	    			"axis": { "labels": false, "ticks": false },
 	    			"sort": {"field": "index"}
 	    		},
 	    		"color": {
-	    			"field": "log2_expression",
+	    			"field": "log2_fold_change",
 	    			//"sort": "descending",
 			      	//"scale": {"scheme": "magma"},
-	    			"type": "quantitative"
+	    			"type": "quantitative",
+	    			"legend":{"title": "Log2 FC"}
 	    		}
 		  	}
 		}, {
 		  	"width": "container",
 		  	"mark": "rect",
 	      	"encoding": {
-	        	"x": {"field": "metacell__name", "axis": {"labels": false, "ticks": false}},
+	        	"x": {"field": "metacell_name", "axis": {"labels": false, "ticks": false}},
 	        	"color": {
-	        		"field": "metacell__color", "legend": false,
-	        		"scale": {"range": {"field": "metacell__color"}}
-	        	},
-	        	"tooltip": [ {"field": "metacell__name"}, {"field": "metacell__type"} ]
+	        		"field": "metacell_color", "legend": false,
+	    			"scale": {"range": {"field": "metacell_color"}}
+	    		},
+	        	"tooltip": [ {"field": "metacell_name"}, {"field": "metacell_type"} ]
 	      	}
 	    }],
 		"config": { "view": { "stroke": "transparent" } }
 	};
     vegaEmbed(id, chart)
-   		.then(res => { expressionHeatmapView = res.view; })
+   		.then(res => { viewExpressionHeatmap = res.view; })
     	.catch(console.error);
 }
