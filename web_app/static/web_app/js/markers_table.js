38138b56e3a43bdb335723a68754960ffe28c2c6
Nuno Agostinho <nuno.agostinho@crg.eu>
2024-12-18 18:13:50 +0100

    Remove rounding of total UMIs

--- web_app/static/web_app/js/markers_table.js
+++ web_app/static/web_app/js/markers_table.js
@@ -43,7 +43,7 @@ function createMarkersTable(id, url, species, metacells, fc_min_type, fc_min, fc
             { data: 'name', title: "Gene ID", render: linkGene },
             { data: 'description', title: "Description", className: 'truncate' },
             { data: 'domains', title: "Domains", render: parseArray, className: 'truncate' },
-            { data: 'fg_sum_umi', title: "Total UMIs", render: round },
+            { data: 'fg_sum_umi', title: "Total UMIs" },
             { data: 'umi_perc', title: "UMI %", render: round },
             { data: 'fg_mean_fc', title: "Mean FC", render: round },
             { data: 'fg_median_fc', title: "Median FC", render: round }
