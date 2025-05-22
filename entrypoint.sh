f38193ccef9f0766a1caddd924050f9550cb3a6e
Nuno Agostinho <nuno.agostinho@crg.eu>
2025-05-22 16:29:08 +0200

    Create tables in production on first run

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,12 @@ set -e
 python manage.py collectstatic --noinput
 
 if [ "${ENVIRONMENT:-}" = "prod" ]; then
+    # Create tables with data models if they do not exist
+    if ! psql -h ${POSTGRES_HOST} -d ${POSTGRES_DB} -tAc "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name LIKE '%_species');" | grep -q t; then
+        python manage.py makemigrations
+        python manage.py migrate
+    fi
+
     # Serve Django apps using gunicorn
     gunicorn -w 4 config.wsgi --bind 0.0.0.0:8000
 else
