b0911e72e911d7d0eeb38fb95e7c9bfa09f221cb
Nuno Agostinho <nuno.agostinho@crg.eu>
2025-05-29 15:00:03 +0200

    Update postgres_init.sql

--- docker-entrypoint-initdb.d/postgres_init.sql
+++ docker-entrypoint-initdb.d/postgres_init.sql
@@ -1,5 +1,9 @@
 /* PostgreSQL initialization script */
 
+-- Increase max_wal_size to reduce how often checkpoints occur
+ALTER SYSTEM SET max_wal_size = '2GB';
+SELECT pg_reload_conf();
+
 -- Activate trigram similarity search
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
