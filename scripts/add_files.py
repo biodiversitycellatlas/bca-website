ff6b4dd28ef7e489b90082ec67962f882ef42238
Nuno Agostinho <nuno.agostinho@crg.eu>
2025-06-20 15:15:47 +0200

    Add multiple scripts

---
+++ scripts/add_files.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import fnmatch
+import os
+import csv
+import functools
+from scripts.utils import load_config, parse_dataset
+from django.core.files import File as DjangoFile
+
+# Auto-flush print statements
+print = functools.partial(print, flush=True)
+
+####### Main functions
+
+config = load_config('data/raw/config.yaml')
+dir  = 'data/raw/files'
+
+def add_file(file_path, species):
+    file_path = str(file_path)
+    if file_path.endswith('.dmnd'):
+        title = "DIAMOND"
+    elif file_path.endswith('.fasta') or file_path.endswith('.fa'):
+        title = "Proteome"
+    else:
+        return
+
+    with open(file_path, 'rb') as f:
+        django_file = DjangoFile(f, name=os.path.basename(file_path))
+        File.objects.get_or_create(
+            species=species, title=title, defaults={'file': django_file})
+
+    return True
+
+for key in config:
+    i = config[key]
+    key = key.split('_')[0] # only need the species part
+    if 'species' not in i.keys():
+        continue
+    species, dataset = parse_dataset(i['species'])
+
+    try:
+        species = Species.objects.get(scientific_name=species)
+    except Species.DoesNotExist:
+        print(f"Warning: species {species} not found")
+        continue
+
+    base_path = Path(dir)
+    for file in base_path.iterdir():
+        if fnmatch.fnmatch(file.name.lower(), f"{key.lower()}*"):
+            print(f"===== {key}: {file} =====")
+            add_file(file, species)
---
+++ scripts/add_gene_lists.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import fnmatch
+import os
+import csv
+import functools
+from scripts.utils import load_config, parse_dataset
+
+# Auto-flush print statements
+print = functools.partial(print, flush=True)
+
+####### Main functions
+
+config = load_config('data/raw/config.yaml')
+data_dir  = 'data/raw'
+lists_dir = f"{data_dir}/gene_lists"
+
+gene_list_map = {
+    "chr": "Chromatin",
+    "ion": "Ion channels",
+    "neu": "Neural",
+    "sig": "Signalling",
+    "rbp": "RNA-binding proteins",
+    "tfs": "Transcription factors",
+    "myo": "Myosins"
+}
+gene_list_map = {
+    acronym: GeneList.objects.get(name=name)
+    for acronym, name in gene_list_map.items()
+}
+
+def update_gene_modules(file_path, species, gene_list):
+    with open(file_path) as file_path:
+        reader = csv.DictReader(file_path, delimiter='\t')
+
+        # Avoid adding same genes to gene list
+        existing_genes = set(gene_list.genes.all())
+
+        g_list = list()
+        for gene in reader:
+            gene = list(gene.values())[0]
+
+            if gene in existing_genes:
+                    continue
+
+            try:
+                g = species.genes.get(name=gene)
+                g_list.append(g)
+            except:
+                print(f"Warning: {gene} has no match in {species}")
+                continue
+
+        gene_list.genes.add(*g_list)
+    return True
+
+all_species = Species.objects.all()
+
+tf_files = []
+
+for key in config:
+    i = config[key]
+    key = key.split('_')[0] # only need the species part
+    if 'species' not in i.keys():
+        continue
+    species, dataset = parse_dataset(i['species'])
+
+    try:
+        species = Species.objects.get(scientific_name=species)
+    except Species.DoesNotExist:
+        print(f"Warning: species {species} not found")
+        continue
+
+    base_path = Path(lists_dir)
+    for file in base_path.iterdir():
+        if fnmatch.fnmatch(file.name.lower(), f"*{key.lower()}*.txt"):
+            print(f"===== {key}: {file} =====")
+
+            # Get gene list from file name
+            acronym = os.path.basename(file_path.name).split(".")[0]
+            genelist = gene_list_map[acronym]
+            print(genelist)
+
+            update_gene_modules(file, species, gene_list)
+
+    if 'tf_annot_file' in i.keys():
+        path = os.path.join(data_dir, i['data_subdir'], i['tf_annot_file'])
+        tf_files.append((species, path))
+
+for f in tf_files:
+    species, file = f
+    update_gene_modules(file, species, gene_list_map["tfs"])
