1cbf4a1a1805be0f8b3c832fcfcd158dd97b9bff
Nuno Agostinho <nuno.agostinho@crg.eu>
2025-07-03 12:50:22 +0200

    Fix downloaded filename suffix

--- app/templatetags/download_links.py
+++ app/templatetags/download_links.py
@@ -2,19 +2,19 @@ from django import template
 
 register = template.Library()
 
-def _build_download_context(viewname, slug, prefix, is_detail):
+def _build_download_context(viewname, slug, suffix, is_detail):
     return {
         'viewname': viewname,
         'slug': slug,
-        'prefix': prefix,
+        'suffix': suffix,
         'formats': ['csv', 'tsv', 'json'],
         'is_detail': is_detail,
     }
 
 @register.inclusion_tag('app/components/links/download_dataset_links.html')
-def download_dataset_list(viewname, slug, prefix):
-    return _build_download_context(viewname, slug, prefix, is_detail=False)
+def download_dataset_list(viewname, slug, suffix):
+    return _build_download_context(viewname, slug, suffix, is_detail=False)
 
 @register.inclusion_tag('app/components/links/download_dataset_links.html')
-def download_dataset_detail(viewname, slug, prefix):
-    return _build_download_context(viewname, slug, prefix, is_detail=True)
+def download_dataset_detail(viewname, slug, suffix):
+    return _build_download_context(viewname, slug, suffix, is_detail=True)
