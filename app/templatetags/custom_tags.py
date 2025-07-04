e99b1a99ba607b12254e23af81a7b8e7fdaf7c54
Nuno Agostinho <nuno.agostinho@crg.eu>
2025-07-04 16:12:53 +0200

    Enhance file_last_modified tag to use current template
    Updated the file_last_modified template tag to default to the current template

--- app/templatetags/custom_tags.py
+++ app/templatetags/custom_tags.py
@@ -5,14 +5,29 @@ import os
 
 register = template.Library()
 
-@register.simple_tag
-def file_last_modified(file_path, format_string="%d %B %Y"):
-    """ Returns the last modified date of a file. """
+@register.simple_tag(takes_context=True)
+def file_last_modified(context, filename=None, format="%d %B %Y"):
+    """
+    Returns the last modified date of a given file or the current template.
+
+    Args:
+        context (dict): Template context.
+        filename (str, optional): Path of file to check (default: current template).
+        format (str): Timestamp format (default: "%d %B %Y").
+
+    Returns:
+        str: Last modified date of the file (or "Unknown" if file is not found).
+    """
+    if filename is None:
+        # Get current file
+        template_name = context.template_name
+        filename = template.loader.get_template(template_name).origin.name
+
     try:
-        timestamp = os.path.getmtime(file_path)
-        return datetime.fromtimestamp(timestamp).strftime(format_string)
+        timestamp = os.path.getmtime(filename)
+        return datetime.fromtimestamp(timestamp).strftime(format)
     except FileNotFoundError:
-        return None
+        return "Unknown"
 
 @register.simple_tag
 def startswith(value, arg):
