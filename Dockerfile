aa8dab3deacbbf82a00287c27f586ccadf9284ca
Nuno Agostinho <nuno.agostinho@crg.eu>
2025-08-05 17:46:55 +0200

    Avoid installing postgresql packages/client

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,9 @@ FROM python:3.13
 
 SHELL ["/bin/bash", "-o", "pipefail", "-c"]
 
-# Install postgresql-client 17 from official repo and diamond-aligner
+# Install diamond-aligner
 RUN apt-get update \
     && apt-get install -y --no-install-recommends \
-            gnupg2=2.2.40-1.1 \
-            lsb-release=12.0-1 \
-    && curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add - \
-    && echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list \
-    && apt-get update \
-    && apt-get install -y --no-install-recommends \
-            postgresql-client-17=17.5-1.pgdg120+1 \
             diamond-aligner=2.1.3-1 \
     && rm -rf /var/lib/apt/lists/*
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,19 +4,46 @@ set -e
 # Collect all static files
 python manage.py collectstatic --noinput
 
+run_django_migrations() {
+    python manage.py makemigrations
+    python manage.py migrate
+}
+
+check_table_exists() {
+    table="%_$1"
+
+    result=$(python -c "
+import psycopg2, os, sys
+conn = psycopg2.connect(
+    host=os.getenv('POSTGRES_HOST'),
+    user=os.getenv('POSTGRES_USER'),
+    password=os.getenv('POSTGRES_PASSWORD'),
+    dbname=os.getenv('POSTGRES_DB')
+)
+cur = conn.cursor()
+cur.execute('''
+    SELECT EXISTS (
+        SELECT 1
+        FROM information_schema.tables
+        WHERE table_name LIKE %s
+    );
+''', (sys.argv[1], ))
+print(1 if cur.fetchone()[0] else 0)
+" "$table")
+    [ "$result" -eq 1 ] && return 0 || return 1
+}
+
 if [ "${ENVIRONMENT:-}" = "prod" ]; then
     # Create tables with data models if they do not exist
-    if ! psql -h ${POSTGRES_HOST} -U ${POSTGRES_USER} -d ${POSTGRES_DB} -tAc "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name LIKE '%_species');" | grep -q t; then
-        python manage.py makemigrations
-        python manage.py migrate
+    if ! check_table_exists 'species'; then
+        run_django_migrations
     fi
 
     # Serve Django apps using gunicorn
     gunicorn -w 4 config.wsgi --bind 0.0.0.0:8000
 else
     # Update data models
-    python manage.py makemigrations
-    python manage.py migrate
+    run_django_migrations
 
     # Run server directly in Django (insecure)
     python manage.py runserver 0.0.0.0:8000
