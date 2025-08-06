11ab1d20d253749b6f12ea629f1e6c3075965917
Nuno Agostinho <nuno.agostinho@crg.eu>
2025-08-06 16:58:05 +0200

    Improve super-linter config for local run

---
+++ super-linter.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check for --fix flag
+for arg in "$@"; do
+  if [ "$arg" == "--fix" ]; then
+    FIX_ENV+=("--env-file" ".github/super-linter-fix.env")
+    break
+  fi
+done
+
+podman run \
+    -e RUN_LOCAL=true \
+    -e LOG_LEVEL=WARN \
+    -e SAVE_SUPER_LINTER_SUMMARY=true \
+    -e SAVE_SUPER_LINTER_OUTPUT=true \
+    --env-file ".github/super-linter.env" \
+    ${FIX_ENV} \
+    -v $(pwd):/tmp/lint \
+    ghcr.io/super-linter/super-linter:latest
