936fb98bdc6428facd602b436f026e70f4076f14
Nuno Agostinho <nuno.agostinho@crg.eu>
2025-08-06 14:37:42 +0200

    Improve comments and database check

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,50 +1,38 @@
 #!/bin/bash
-set -e
+set -e # Exit if any command returns a non-zero status
 
-# Collect all static files
+# Collect all Django static files
 python manage.py collectstatic --noinput
 
+# Create and apply Django migrations
 run_django_migrations() {
     python manage.py makemigrations
     python manage.py migrate
 }
 
-check_table_exists() {
+# Check if table exists in database
+has_table() {
     table="%_$1"
 
-    result=$(python -c "
-import psycopg2, os, sys
-conn = psycopg2.connect(
-    host=os.getenv('POSTGRES_HOST'),
-    user=os.getenv('POSTGRES_USER'),
-    password=os.getenv('POSTGRES_PASSWORD'),
-    dbname=os.getenv('POSTGRES_DB')
-)
-cur = conn.cursor()
-cur.execute('''
-    SELECT EXISTS (
-        SELECT 1
-        FROM information_schema.tables
-        WHERE table_name LIKE %s
-    );
-''', (sys.argv[1], ))
-print(1 if cur.fetchone()[0] else 0)
-" "$table")
-    [ "$result" -eq 1 ] && return 0 || return 1
+    psql -h ${POSTGRES_HOST} -U ${POSTGRES_USER} -d ${POSTGRES_DB} -tAc \
+    "SELECT EXISTS (
+        SELECT 1 FROM information_schema.tables WHERE table_name LIKE '${table}'
+    );" | grep -q t
 }
 
+# Check whether the environment is production
 if [ "${ENVIRONMENT:-}" = "prod" ]; then
     # Create tables with data models if they do not exist
-    if ! check_table_exists 'species'; then
+    if ! has_table 'species'; then
         run_django_migrations
     fi
 
     # Serve Django apps using gunicorn
     gunicorn -w 4 config.wsgi --bind 0.0.0.0:8000
 else
-    # Update data models
+    # Update data models in dev
     run_django_migrations
 
-    # Run server directly in Django (insecure)
+    # Run server directly in Django (insecure, dev only)
     python manage.py runserver 0.0.0.0:8000
 fi
