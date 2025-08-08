f79945b76f7bb16fde59d34f30ba195e727275ca
Nuno Agostinho <nuno.agostinho@crg.eu>
2025-08-08 16:07:08 +0200

    Lint with prettier

--- app/static/app/js/tooltips.js
+++ app/static/app/js/tooltips.js
@@ -7,18 +7,22 @@
 
 // tooltips.js
 export function enableTooltipsAndPopovers() {
-	// Allow table elements in tooltips
+    // Allow table elements in tooltips
     bootstrap.Tooltip.Default.allowList.table = [];
     bootstrap.Tooltip.Default.allowList.thead = [];
     bootstrap.Tooltip.Default.allowList.tbody = [];
     bootstrap.Tooltip.Default.allowList.tr = [];
     bootstrap.Tooltip.Default.allowList.td = [];
 
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-    tooltipTriggerList.forEach(el => new bootstrap.Tooltip(el));
+    const tooltipTriggerList = document.querySelectorAll(
+        '[data-bs-toggle="tooltip"]',
+    );
+    tooltipTriggerList.forEach((el) => new bootstrap.Tooltip(el));
 
-    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
-    popoverTriggerList.forEach(el => new bootstrap.Popover(el));
+    const popoverTriggerList = document.querySelectorAll(
+        '[data-bs-toggle="popover"]',
+    );
+    popoverTriggerList.forEach((el) => new bootstrap.Popover(el));
 }
 
 // call it immediately
