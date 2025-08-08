72239d089ab5e30ea815cda7a6eacd5965661a21
Nuno Agostinho <nuno.agostinho@crg.eu>
2025-08-08 16:06:22 +0200

    Use module for JS script

--- app/static/app/js/tooltips.js
+++ app/static/app/js/tooltips.js
@@ -3,15 +3,23 @@
  * Also allows table HTML tags in tooltips.
  */
 
-// Allow table elements in tooltips
-bootstrap.Tooltip.Default.allowList.table = [];
-bootstrap.Tooltip.Default.allowList.thead = [];
-bootstrap.Tooltip.Default.allowList.tbody = [];
-bootstrap.Tooltip.Default.allowList.tr = [];
-bootstrap.Tooltip.Default.allowList.td = [];
+/* global bootstrap */
 
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(el => new bootstrap.Tooltip(el))
+// tooltips.js
+export function enableTooltipsAndPopovers() {
+	// Allow table elements in tooltips
+    bootstrap.Tooltip.Default.allowList.table = [];
+    bootstrap.Tooltip.Default.allowList.thead = [];
+    bootstrap.Tooltip.Default.allowList.tbody = [];
+    bootstrap.Tooltip.Default.allowList.tr = [];
+    bootstrap.Tooltip.Default.allowList.td = [];
 
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(el => new bootstrap.Popover(el))
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    tooltipTriggerList.forEach(el => new bootstrap.Tooltip(el));
+
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+    popoverTriggerList.forEach(el => new bootstrap.Popover(el));
+}
+
+// call it immediately
+enableTooltipsAndPopovers();
