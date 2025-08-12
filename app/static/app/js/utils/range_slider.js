062640fb8812e1d7e74d9d8efd054954e16da452
Nuno Agostinho <nuno.agostinho@crg.eu>
2025-08-12 12:20:23 +0200

    Document functions

--- app/static/app/js/utils/range_slider.js
+++ app/static/app/js/utils/range_slider.js
@@ -2,6 +2,14 @@
  * Utility functions for range sliders.
  */
 
+/**
+ * Update text content and ionRangeSlider settings.
+ *
+ * @param {string} id - Selector for the element whose text will be updated.
+ * @param {string|null} from_min_id - Selector for the ionRangeSlider to update `from_min` value. If null, skip update.
+ * @param {string} [suffix=""] - Optional suffix appended to the displayed text.
+ * @returns {function} - A callback function that takes a `data` object with a `from` property.
+ */
 function updateText(id, from_min_id, suffix="") {
     return function update(data) {
         $(id).text(data.from + suffix);
@@ -13,6 +21,14 @@ function updateText(id, from_min_id, suffix="") {
     }
 }
 
+/**
+ * Initializes an ionRangeSlider on the given selector with base and custom options,
+ * optionally linking text update callbacks.
+ *
+ * @param {string} selector - jQuery selector for the slider element(s).
+ * @param {object} opts - Custom ionRangeSlider options to override base settings.
+ * @param {Array} [textArgs] - Optional array of arguments to pass to `updateText` to create callbacks.
+ */
 export function initRangeSlider(selector, opts, textArgs) {
     const baseOpts = { grid: true, skin: "round" };
     let cb;
