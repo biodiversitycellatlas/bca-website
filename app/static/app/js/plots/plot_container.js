166dd0da98620d3c02b25b413c76aaefd7767972
Nuno Agostinho <nuno.agostinho@crg.eu>
2025-08-18 18:44:13 +0200

    Update plot_container.js

--- app/static/app/js/plots/plot_container.js
+++ app/static/app/js/plots/plot_container.js
@@ -1,10 +1,26 @@
+/**
+ * Loading spinners in plot containers.
+ *
+ * Expects container elements to follow the convention:
+ *   - Plot container:   <id>-plot
+ *   - Spinner element:  <id>-spinner
+ */
+
 /* global $ */
 
-// Loading spinner functions
+/**
+ * Get the spinner element for a given container id.
+ * @param {string} id - Base id of the container.
+ * @returns {HTMLElement|null} Spinner element, or null if not found.
+ */
 function getSpinner(id) {
     return document.getElementById(id + "-spinner");
 }
 
+/**
+ * Hide the spinner inside a container.
+ * @param {string} id - Base id of the container.
+ */
 export function hideSpinner(id) {
     var spinner = getSpinner(id);
     if (spinner !== null) {
@@ -12,6 +28,10 @@ export function hideSpinner(id) {
     }
 }
 
+/**
+ * Show the spinner inside a container.
+ * @param {string} id - Base id of the container.
+ */
 export function showSpinner(id) {
     var spinner = getSpinner(id);
     if (spinner !== null) {
@@ -19,9 +39,14 @@ export function showSpinner(id) {
     }
 }
 
-// Reset container with a plot to show spinner
+/**
+ * Clear a plot container by inserting a loading spinner.
+ * @param {string} id - Base id of the container.
+ */
 export function clearContainer(id) {
     $("#" + id + "-plot").html(
-        `<div id="${id}-spinner" class="loading-state"><div class="loader"></div></div>`,
+        `<div id="${id}-spinner" class="loading-state">
+            <div class="loader"></div>
+        </div>`,
     );
 }
