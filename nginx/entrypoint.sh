a55aafb77520bbff9e13cfbabedf5561ca8232a4
Nuno Agostinho <nuno.agostinho@crg.eu>
2025-11-05 16:06:09 +0100

    Add env varibles for SSL certificate

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -14,5 +14,12 @@ envsubst '$DJANGO_HOSTNAME $GHOST_HOSTNAME' \
 echo "[NGINX] Final NGINX config:"
 cat /etc/nginx/conf.d/default.conf
 
+# Ensure nginx cert folder exists
+mkdir -p /etc/nginx/certs
+
+# Link the cert and key dynamically from env vars
+ln -sf "/certs/$CERT_FILE" /etc/nginx/certs/server.crt
+ln -sf "/certs/$CERT_KEY_FILE" /etc/nginx/certs/server.key
+
 echo "[NGINX] Starting server..."
 exec nginx -g 'daemon off;'
