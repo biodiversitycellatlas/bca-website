946343aecac84cdcda1e875fa9cfee461d916a1f
Andrés Becerra <andres.becerra@crg.eu>
2025-11-26 15:12:32 +0100

    Add test for expression in hdf5 file

--- rest/tests.py
+++ rest/tests.py
@@ -1,7 +1,10 @@
+import os.path
+
+from django.core.files import File as DjangoFile
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from app.models import Species, Dataset, Gene
+from app.models import Species, Dataset, Gene, SingleCell, Metacell, MetacellType, DatasetFile
 
 
 class SpeciesTests(APITestCase):
@@ -42,7 +45,7 @@ class DatasetTests(APITestCase):
         self.assertSetEqual({s["dataset"] for s in datasets}, {"DRat", "DMouse"})
 
 
-class GenesTests(APITestCase):
+class GeneTests(APITestCase):
     """Test Genes Endpoint"""
 
     def setUp(self):
@@ -59,3 +62,43 @@ class GenesTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(genes), 2)
         self.assertSetEqual({s["name"] for s in genes}, {"Gene1", "Gene2"})
+
+
+class SingleCellGeneExpressionTests(APITestCase):
+    """Tests SingleCellGeneExpression Endpoint"""
+
+    def setUp(self):
+        species1 = Species.objects.create(common_name="rat", scientific_name="Rat", description="rat")
+        species1.save()
+        dataset1 = Dataset.objects.create(species=species1, name="DRat", description="rat dataset")
+        dataset1.save()
+        self.dataset_id = dataset1.pk
+        type1 = MetacellType.objects.create(name="type1", dataset=dataset1)
+        type1.save()
+        metacell1 = Metacell.objects.create(name="meta1", dataset=dataset1, type=type1, x=3, y=5)
+
+        for i in range(1, 5):
+            gene = "g" + str(i)
+            Gene.objects.create(name=gene, species=species1).save()
+        self.gene_id = Gene.objects.all().first().pk
+
+        for i in range(1, 6):
+            cell = "c" + str(i)
+            SingleCell.objects.create(name=cell, dataset=dataset1, metacell=metacell1).save()
+
+        test_file = os.path.join(os.path.dirname(__file__), "test_fixtures", "gene_expression_test.hdf5")
+        with open(test_file, "rb") as f:
+            django_file = DjangoFile(f, name=os.path.basename(test_file))
+            DatasetFile.objects.get_or_create(
+                dataset=dataset1, type="singlecell_umifrac", defaults={"file": django_file}
+            )
+
+    def test_retrieve(self):
+        url = f"/api/v1/single_cell_expression/?dataset={self.dataset_id}&gene={self.gene_id}"
+        response = self.client.get(url, format="json")
+        expression_values = response.data
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(expression_values), 2)
+        self.assertSetEqual({s["single_cell"] for s in expression_values}, {"c3", "c5"})
+        self.assertSetEqual({s["umifrac"] for s in expression_values}, {2142.857177734375, 10000.0})
+        pass
