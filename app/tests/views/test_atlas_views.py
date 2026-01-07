30077ae9124c3a0ef85519053b06428e2d4d77ff
Nuno Agostinho <nuno.agostinho@crg.eu>
2026-01-07 16:28:55 +0100

    Improve unit tests on views

--- app/tests/views/test_atlas_views.py
+++ app/tests/views/test_atlas_views.py
@@ -1,9 +1,8 @@
-from django.test import TestCase, RequestFactory
+from django.test import TestCase, Client
 from django.urls import reverse
-from types import SimpleNamespace
 
-from app.views import AtlasView, BaseAtlasView, AtlasInfoView
-from app.models import Dataset, Species
+from app.views import AtlasView
+from app.models import Dataset, Species, Gene
 
 
 class BaseTestCase(TestCase):
@@ -13,67 +12,103 @@ class BaseTestCase(TestCase):
         cls.species = Species.objects.create(scientific_name="Mus musculus", common_name="mouse")
         cls.dataset = Dataset.objects.create(name="adult", species=cls.species)
 
-    def setUp(self):
-        self.factory = RequestFactory()
-
-    def get_atlas_request(self, url="/atlas/"):
-        request = self.factory.get(url)
-        request.resolver_match = SimpleNamespace(url_name="atlas")
-        return request
+        cls.brca1 = Gene.objects.create(name="Brca1", species=cls.species)
+        cls.brca2 = Gene.objects.create(name="Brca2", species=cls.species)
+        cls.client = Client()
 
 
 class AtlasViewTest(BaseTestCase):
     def test_atlas_view_basic(self):
-        request = self.get_atlas_request()
-        response = AtlasView.as_view()(request)
-
+        response = self.client.get("/atlas/")
         self.assertEqual(response.status_code, 200)
-        self.assertIn("icon", response.context_data)
-        self.assertIn("dataset_dict", response.context_data)
-        self.assertIn("links", response.context_data)
+        self.assertIn("icon", response.context)
+        self.assertIn("dataset_dict", response.context)
+        self.assertIn("links", response.context)
 
     def test_get_species_icon_random(self):
         icon = AtlasView().get_species_icon()
         self.assertIsInstance(icon, str)
 
     def test_invalid_dataset_warning(self):
-        request = self.get_atlas_request("/atlas/?dataset=invalid-dataset")
-        response = AtlasView.as_view()(request)
-
-        self.assertIn("warning", response.context_data)
-        self.assertIn("Invalid dataset", response.context_data["warning"]["title"])
-
-
-class BaseAtlasViewTest(BaseTestCase):
-    def test_valid_dataset_context(self):
-        request = self.get_atlas_request(f"/atlas/{self.dataset}/")
-        response = BaseAtlasView.as_view()(request, dataset=self.dataset)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context_data["dataset"], self.dataset)
-        self.assertEqual(response.context_data["species"].common_name, "mouse")
-        self.assertIn("dataset_dict", response.context_data)
-        self.assertIn("links", response.context_data)
+        response = self.client.get("/atlas/?dataset=invalid-dataset")
+        self.assertIn("warning", response.context)
+        self.assertIn("Invalid dataset", response.context["warning"]["title"])
 
     def test_invalid_dataset_redirect(self):
-        request = self.factory.get("/atlas/invalid/")
-        response = BaseAtlasView.as_view()(request, dataset="invalid")
-
+        response = self.client.get("/atlas/invalid/")
         self.assertEqual(response.status_code, 302)
         self.assertIn(reverse("atlas"), response.url)
 
 
 class AtlasInfoViewTest(BaseTestCase):
-    def test_context_data_with_valid_dataset(self):
-        request = self.factory.get("/atlas/{self.dataset}/")
-        request.resolver_match = SimpleNamespace(url_name="atlas_info")
+    def test_dataset(self):
+        response = self.client.get(f"/atlas/{self.dataset.slug}/")
+        self.assertEqual(response.status_code, 200)
 
-        response = AtlasInfoView.as_view()(request, dataset=self.dataset)
-        context = response.context_data
-
+        context = response.context
         self.assertEqual(context["dataset"], self.dataset)
-        self.assertIn("qc_metrics", context)
+        self.assertEqual(context["species"].common_name, "mouse")
+        self.assertIn("dataset_dict", context)
+        self.assertIn("links", context)
 
+        # Test quality control metrics
+        self.assertIn("qc_metrics", context)
         for metric in context["qc_metrics"]:
             self.assertIn("title", metric)
             self.assertIn("values", metric)
+
+
+class AtlasOverviewViewTest(BaseTestCase):
+    def test_dataset(self):
+        response = self.client.get(f"/atlas/{self.dataset.slug}/overview/")
+        self.assertEqual(response.status_code, 200)
+
+
+class AtlasGeneViewTest(BaseTestCase):
+    def test_index(self):
+        response = self.client.get(f"/atlas/{self.dataset.slug}/gene/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("gene", response.context)
+        self.assertEqual(response.context["gene"], "")
+        self.assertNotIn("warning", response.context)
+
+    def test_valid_gene(self):
+        response = self.client.get(f"/atlas/{self.dataset.slug}/gene/Brca1/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("gene", response.context)
+        self.assertIsInstance(response.context["gene"], Gene)
+        self.assertEqual(response.context["gene"].name, "Brca1")
+
+        # No warnings expected
+        self.assertNotIn("warning", response.context)
+
+    def test_invalid_gene(self):
+        response = self.client.get(f"/atlas/{self.dataset.slug}/gene/invalid-gene/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["gene"], "")
+        self.assertIn("warning", response.context)
+
+        # Raise warning for invalid gene
+        warning = response.context["warning"]
+        self.assertIn("Invalid gene", warning["title"])
+        self.assertIn("Please check available genes", warning["description"])
+
+
+class AtlasPanelViewTest(BaseTestCase):
+    def test_gene_panel(self):
+        response = self.client.get(f"/atlas/{self.dataset.slug}/panel/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("metacell_dict", response.context)
+
+
+class AtlasMarkersViewTest(BaseTestCase):
+    def test_gene_markers(self):
+        response = self.client.get(f"/atlas/{self.dataset.slug}/markers/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("metacell_dict", response.context)
+
+
+class AtlasCompareViewTest(BaseTestCase):
+    def test_gene_markers(self):
+        response = self.client.get(f"/atlas/{self.dataset.slug}/compare/")
+        self.assertEqual(response.status_code, 200)
--- app/tests/views/test_views.py
+++ app/tests/views/test_views.py
@@ -1,82 +1,139 @@
 import ssl
 import io
-from contextlib import redirect_stdout, redirect_stderr
-from django.test import TestCase, RequestFactory, override_settings
-from django.http import Http404
+import os
 
-from app.views import IndexView, DocumentationView
+from contextlib import redirect_stdout, redirect_stderr
+from datetime import datetime
+
+from django.test import TestCase, Client, override_settings
+from django.conf import settings
+
+from app.views import DocumentationView
+from app.tests.views.test_atlas_views import BaseTestCase
 
 
 @override_settings(GHOST_INTERNAL_URL="https://biodiversitycellatlas.org")
 class IndexViewTest(TestCase):
-    def setUp(self):
-        self.factory = RequestFactory()
-
+    @classmethod
+    def setUpTestData(cls):
         # Disable SSL verification to fetch data
         ssl._create_default_https_context = ssl._create_unverified_context
+        cls.client = Client()
 
     def test_homepage_context(self):
         # Suppress stdout/stderr during feed fetch to avoid 404 errors
         f = io.StringIO()
         with redirect_stdout(f), redirect_stderr(f):
-            request = self.factory.get("/")
-            response = IndexView.as_view()(request)
-            response.render()
+            response = self.client.get("/")
 
-        self.assertIn("dataset_dict", response.context_data)
-        self.assertIn("posts", response.context_data)
+        self.assertIn("dataset_dict", response.context)
+        self.assertIn("posts", response.context)
 
         # Check context keys
-        self.assertIn("dataset_dict", response.context_data)
-        self.assertIn("posts", response.context_data)
+        self.assertIn("dataset_dict", response.context)
+        self.assertIn("posts", response.context)
 
         # Check posts keys
         categories = [None, "publications", "meetings", "tutorials"]
-        self.assertEqual(set(response.context_data["posts"].keys()), set(categories))
+        self.assertEqual(set(response.context["posts"].keys()), set(categories))
 
         # Check dataset_dict is a dict
-        self.assertIsInstance(response.context_data["dataset_dict"], dict)
+        self.assertIsInstance(response.context["dataset_dict"], dict)
 
         # Check response contains some text
         self.assertIn("<body", response.content.decode())
 
 
 class DocumentationViewTest(TestCase):
-    def setUp(self):
-        self.factory = RequestFactory()
-        self.docs_dir = DocumentationView.docs_dir
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = Client()
+        cls.docs_dir = DocumentationView.docs_dir
 
-    def test_get_docs_index(self):
-        request = self.factory.get("/docs/")
-        response = DocumentationView.as_view()(request, page="_index")
-        self.assertIn("<ul>", response.context_data["index"])
-
-    def test_get_docs_dir(self):
-        request = self.factory.get("/docs/tutorials/")
-        response = DocumentationView.as_view()(request, page="tutorials")
-
+    def test_docs_index(self):
+        response = self.client.get("/docs/")
         self.assertEqual(response.status_code, 200)
-        self.assertIn("List of tutorials", response.context_data["content"])
-        self.assertIn("Tutorials", response.context_data["index"])
+        self.assertIn("<ul>", response.context["index"])
+
+    def test_docs_dir(self):
+        response = self.client.get("/docs/tutorials/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("List of tutorials", response.context["content"])
+        self.assertIn("Tutorials", response.context["index"])
 
         # Test breadcrumbs
-        response.render()
-        self.assertIn("Tutorials", response.content.decode())
+        self.assertIn("breadcrumb-nav", response.content.decode())
 
-    def test_get_docs_page(self):
-        request = self.factory.get("/docs/tutorials/metacell.md")
-        response = DocumentationView.as_view()(request, page="tutorials/metacell")
-
+    def test_docs_page(self):
+        response = self.client.get("/docs/tutorials/metacell/")
         self.assertEqual(response.status_code, 200)
-        self.assertIn("Metacell tutorial", response.context_data["content"])
-        self.assertIn("Metacells", response.context_data["index"])
+        self.assertIn("Metacell tutorial", response.context["content"])
+        self.assertIn("Metacells", response.context["index"])
 
-        metadata = response.context_data["metadata"]
+        metadata = response.context["metadata"]
         self.assertIn("title", metadata.keys())
         self.assertIn("linkTitle", metadata.keys())
 
-    def test_get_context_data_404(self):
+    def test_404(self):
         # Test 404 on non-existing page
-        request = self.factory.get("/docs/random-page/")
-        with self.assertRaises(Http404):
-            DocumentationView.as_view()(request, page="random-page")
+        response = self.client.get("/docs/random-page/")
+        self.assertEqual(response.status_code, 404)
+
+
+class AboutViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.response = self.client.get("/about/")
+
+    def test_about_view_context(self):
+        # Check that context contains "info"
+        self.assertIn("info", self.response.context)
+        info = self.response.context["info"]
+
+        # Check main sections exist
+        self.assertIn("contact", info)
+        self.assertIn("legal", info)
+        self.assertIn("licenses", info)
+
+        # Check that contact section contains the Email entry
+        email_entry = info["contact"][0]
+        self.assertEqual(email_entry["url"], settings.FEEDBACK_URL)
+
+        # Check licenses section
+        fa_entry = next((x for x in info["licenses"] if "Font Awesome" in x["label"]), None)
+        self.assertIsNotNone(fa_entry)
+
+        rubik_entry = next((x for x in info["licenses"] if "Rubik font" in x["label"]), None)
+        self.assertIsNotNone(rubik_entry)
+
+        # Check URLs
+        for section in info.values():
+            for item in section:
+                self.assertIsInstance(item["url"], str)
+                self.assertIsInstance(item["label"], str)
+                self.assertIsInstance(item["icon"], str)
+
+    def test_last_updated(self):
+        # Check if last modified time for template is correctly being used
+        template = self.response.templates[0].origin.name
+        mtime = os.path.getmtime(template)
+        mtime_str = datetime.fromtimestamp(mtime).strftime("%d %B %Y")
+
+        content = self.response.content.decode()
+        self.assertIn("Last updated", content)
+        self.assertIn(mtime_str, content)
+
+
+class SearchViewTest(BaseTestCase):
+    def test_search_view_context_without_query(self):
+        response = self.client.get("/search/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("species_dict", response.context)
+        self.assertNotIn("query", response.context)
+
+    def test_search_view_context_with_query(self):
+        response = self.client.get("/search/", {"q": "test"})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("species_dict", response.context)
+        self.assertIn("query", response.context)
+        self.assertEqual(response.context["query"]["q"], "test")
