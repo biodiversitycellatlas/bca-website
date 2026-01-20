1acb5cb29c92d0091f537a641152910a5924dff7
Andrés Becerra Sandoval <andres.becerra@gmail.com>
2026-01-20 16:56:42 +0100

    Update rest/tests.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- rest/tests.py
+++ rest/tests.py
@@ -364,10 +364,7 @@ class AlignTests(APITestCase):
         test_file = os.path.join(os.path.dirname(__file__), "test_fixtures", "test-dmd-db.dmnd")
         with open(test_file, "rb") as f:
             django_file = DjangoFile(f, name=os.path.basename(test_file))
-            SpeciesFile.objects.get_or_create(
-                species=species1,
-                type="DIAMOND",
-                defaults={"file": django_file})
+            SpeciesFile.objects.get_or_create(species=species1, type="DIAMOND", defaults={"file": django_file})
 
     def check_expected_alignment(self, response):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
