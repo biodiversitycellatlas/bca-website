6cb5aa461d7e7a4b70def28e0bc2a27e72567e5e
Andrés Becerra <andres.becerra@crg.eu>
2026-01-20 16:46:36 +0100

    Linting

--- rest/tests.py
+++ rest/tests.py
@@ -358,11 +358,18 @@ class AlignTests(APITestCase):
 
     @classmethod
     def setUpTestData(cls):
-        species1 = Species.objects.create(common_name="aligner", scientific_name="Alignspecies", description="Align Species")
-        test_file = os.path.join(os.path.dirname(__file__), "test_fixtures", "test-dmd-db.dmnd")
+        species1 = Species.objects.create(
+            common_name="aligner",
+            scientific_name="Alignspecies",
+            description="Align Species")
+        test_file = os.path.join(
+            os.path.dirname(__file__), "test_fixtures", "test-dmd-db.dmnd")
         with open(test_file, "rb") as f:
             django_file = DjangoFile(f, name=os.path.basename(test_file))
-            SpeciesFile.objects.get_or_create(species=species1, type="DIAMOND", defaults={"file": django_file})
+            SpeciesFile.objects.get_or_create(
+                species=species1,
+                type="DIAMOND",
+                defaults={"file": django_file})
 
     def check_expected_alignment(self, response):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
