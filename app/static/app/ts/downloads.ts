5484bbbfab2fa16423df0c74bb9de6ad23c01273
Nuno Agostinho <nuno.agostinho@crg.eu>
2026-01-22 16:37:02 +0100

    Use vanilla JS in templates

---
+++ app/static/app/ts/downloads.ts
@@ -0,0 +1,24 @@
+/**
+ * Download page functions.
+ */
+
+import $ from "jquery";
+import "datatables.net-bs5";
+
+
+/**
+ * Render DataTables for downloading data.
+ *
+ * @param {Object} obj - Hierarchical tree object
+ * @returns {Array} Flattened array with nodes containing id, parent, name, and size
+ */
+export function renderTables(id) {
+    $(id).DataTable({
+        responsive: true,
+        pageLength: 25,
+        scrollX: true,
+        language: { "search": "" }
+    });
+
+    $(".dt-search input").attr("placeholder", "Search table...");
+}
