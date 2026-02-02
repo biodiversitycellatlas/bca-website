4bf0ad01172aeb3e9eaeb2bf43632c48a775c741
Andrés Becerra <andres.becerra@crg.eu>
2026-02-02 17:42:29 +0100

    122: only run in prod

--- app/systemchecks/postgresql_tables.py
+++ app/systemchecks/postgresql_tables.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from django.apps import apps
+from django.conf import settings
 from django.core.checks import Error, register
 from django.core import checks
 
@@ -11,12 +12,13 @@ MIN_NUM_RECORDS = 4
 @register(checks.Tags.files, deploy=True)
 def check_tables(app_configs, **kwargs) -> List[Error]:
     errors = []
-    models = apps.get_app_config("app").models
+    if not settings.DEBUG:
+        models = apps.get_app_config("app").models
 
-    for model in models:
-        if model != "singlecellgeneexpression":
-            n = len(apps.get_model(f"app.{model}").objects.all()[:MIN_NUM_RECORDS])
-            if n < MIN_NUM_RECORDS:
-                errors.append(Error(msg="Table has not enough data", hint="few records", obj=model, id="bca.E003"))
+        for model in models:
+            if model != "singlecellgeneexpression":
+                n = len(apps.get_model(f"app.{model}").objects.all()[:MIN_NUM_RECORDS])
+                if n < MIN_NUM_RECORDS:
+                    errors.append(Error(msg="Table has not enough data", hint="few records", obj=model, id="bca.E003"))
 
     return errors
