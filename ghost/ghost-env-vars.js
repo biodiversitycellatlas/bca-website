a7ab2b7c8464b937d92b6cb899598f54a655189c
Clemente Borges <clemenbor@gmail.com>
2026-02-11 16:49:21 +0100

    Fix javascript prettier

--- ghost/ghost-env-vars.js
+++ ghost/ghost-env-vars.js
@@ -1,15 +1,15 @@
 // Patch Ghost theme's package.json to expose environment variables via @custom config
 
-const fs = require('fs');
+const fs = require("fs");
 
-const path = 'content.orig/themes/source/package.json';
+const path = "content.orig/themes/source/package.json";
 const j = JSON.parse(fs.readFileSync(path));
 
 j.config = j.config || {};
 j.config.custom = j.config.custom || {};
 j.config.custom.tracking_script = {
     type: "text",
-    default: process.env.PLAUSIBLE_SCRIPT_URL
+    default: process.env.PLAUSIBLE_SCRIPT_URL,
 };
 
 fs.writeFileSync(path, JSON.stringify(j, null, 2));
