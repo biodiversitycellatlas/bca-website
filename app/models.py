f19637dccb417d0ed48b41d2c166de5263f7ed64
Nuno Agostinho <nuno.agostinho@crg.eu>
2026-02-20 16:03:06 +0100

    Lint code
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/models.py
+++ app/models.py
@@ -10,7 +10,6 @@ from django.db import models
 from django.urls import reverse
 from django.utils.safestring import mark_safe
 from django.utils.text import slugify
-from django.core.exceptions import ValidationError
 
 
 class AutoSlugMixin(models.Model):
