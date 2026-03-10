91470c4883f4f1d164dab948673fcf5cd993efd2
Nuno Agostinho <nuno.agostinho@crg.eu>
2026-03-10 16:53:18 +0100

    Copy-edit comment

--- app/views/entry_views.py
+++ app/views/entry_views.py
@@ -205,7 +205,7 @@ class OrthogroupDetailView(ListView):
     template_name = "app/entries/orthogroup_detail.html"
 
     def get_queryset(self):
-        """Filter queryset by domain."""
+        """Filter queryset by orthogroup."""
         qs = super().get_queryset()
         orthogroup = self.kwargs.get("orthogroup")
         return qs.filter(orthogroup__name=orthogroup)
