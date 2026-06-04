a0a915640d82e293705d1e5e732ae1dab3bbf080
Andrés Becerra <andres.becerra@crg.eu>
2026-06-04 12:11:25 +0200

    Change rest/tests and test a bandit ignore line

--- rest/tests/tests.py
+++ rest/tests/tests.py
@@ -36,17 +36,17 @@ class SpeciesTests(APITestCase):
     def test_retrieve(self):
         response = self.client.get("/api/v1/species/", format="json")
         species = response.data["results"]
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(species), 2)
-        self.assertSetEqual({s["common_name"] for s in species}, {"rat", "mouse"})
+        assert response.status_code == status.HTTP_200_OK
+        assert len(species) == 2
+        assert {s["common_name"] for s in species} == {"rat", "mouse"}
 
     def test_get(self):
         response = self.client.get("/api/v1/species/Rat/", format="json")
         species = dict(response.data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(species["common_name"], "rat")
-        self.assertEqual(species["scientific_name"], "Rat")
-        self.assertEqual(species["description"], "rat")
+        assert response.status_code == status.HTTP_200_OK
+        assert species["common_name"] == "rat"
+        assert species["scientific_name"] == "Rat"
+        assert species["description"] == "rat"
 
 
 class DatasetTests(APITestCase):
@@ -62,36 +62,36 @@ class DatasetTests(APITestCase):
     def test_retrieve(self):
         response = self.client.get("/api/v1/datasets/", format="json")
         datasets = response.data["results"]
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(datasets), 2)
-        self.assertSetEqual({s["dataset"] for s in datasets}, {"DRat", "DMouse"})
+        assert response.status_code == status.HTTP_200_OK
+        assert len(datasets) == 2
+        assert {s["dataset"] for s in datasets} == {"DRat", "DMouse"}
 
     def test_get(self):
         response = self.client.get("/api/v1/datasets/mouse-dmouse/", format="json")
         dataset = dict(response.data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(dataset["slug"], "mouse-dmouse")
-        self.assertEqual(dataset["dataset"], "DMouse")
-        self.assertEqual(dataset["species"], "Mouse")
+        assert response.status_code == status.HTTP_200_OK
+        assert dataset["slug"] == "mouse-dmouse"
+        assert dataset["dataset"] == "DMouse"
+        assert dataset["species"] == "Mouse"
 
     def test_retrieve_stats(self):
         response = self.client.get("/api/v1/stats/", format="json")
         datasets_stats = response.data["results"]
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertSetEqual({s["species"] for s in datasets_stats}, {"Mouse", "Rat"})
-        self.assertSetEqual({s["dataset"] for s in datasets_stats}, {"DMouse", "DRat"})
-        self.assertSetEqual({s["genes"] for s in datasets_stats}, {0, 0})
-        self.assertSetEqual({s["cells"] for s in datasets_stats}, {0, 0})
-        self.assertSetEqual({s["metacells"] for s in datasets_stats}, {0, 0})
+        assert response.status_code == status.HTTP_200_OK
+        assert {s["species"] for s in datasets_stats} == {"Mouse", "Rat"}
+        assert {s["dataset"] for s in datasets_stats} == {"DMouse", "DRat"}
+        assert {s["genes"] for s in datasets_stats} == {0, 0}
+        assert {s["cells"] for s in datasets_stats} == {0, 0}
+        assert {s["metacells"] for s in datasets_stats} == {0, 0}
 
     def test_get_stats(self):
         response = self.client.get("/api/v1/stats/rat-drat/", format="json")
         dataset_stats = dict(response.data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(dataset_stats["dataset"], "DRat")
-        self.assertEqual(dataset_stats["species"], "Rat")
-        self.assertEqual(dataset_stats["genes"], 0)
-        self.assertEqual(dataset_stats["cells"], 0)
+        assert response.status_code == status.HTTP_200_OK
+        assert dataset_stats["dataset"] == "DRat"
+        assert dataset_stats["species"] == "Rat"
+        assert dataset_stats["genes"] == 0
+        assert dataset_stats["cells"] == 0
 
 
 class GeneTests(APITestCase):
@@ -106,9 +106,9 @@ class GeneTests(APITestCase):
     def test_genes(self):
         response = self.client.get("/api/v1/genes/", format="json")
         genes = response.data["results"]
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(genes), 2)
-        self.assertSetEqual({s["gene"] for s in genes}, {"Gene1", "Gene2"})
+        assert response.status_code == status.HTTP_200_OK
+        assert len(genes) == 2
+        assert {s["gene"] for s in genes} == {"Gene1", "Gene2"}
 
 
 @override_settings(MEDIA_ROOT=tempfile.mkdtemp())
@@ -142,14 +142,14 @@ class SingleCellGeneExpressionTests(APITestCase):
         url = "/api/v1/single_cell_expression/?dataset=rat-drat&gene=g1"
         response = self.client.get(url, format="json")
         expression_values = response.data
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(expression_values), 2)
-        self.assertSetEqual({s["single_cell"] for s in expression_values}, {"c3", "c5"})
+        assert response.status_code == status.HTTP_200_OK
+        assert len(expression_values) == 2
+        assert {s["single_cell"] for s in expression_values} == {"c3", "c5"}
         for entry in expression_values:
             if entry["single_cell"] == "c3":
-                self.assertTrue(math.isclose(float(entry["umifrac"]), 2142.857, rel_tol=0.001))
+                assert math.isclose(float(entry["umifrac"]), 2142.857, rel_tol=0.001)
             if entry["single_cell"] == "c5":
-                self.assertTrue(math.isclose(float(entry["umifrac"]), 10000, rel_tol=0.001))
+                assert math.isclose(float(entry["umifrac"]), 10000, rel_tol=0.001)
 
 
 class SingleCellTests(APITestCase):
@@ -167,9 +167,9 @@ class SingleCellTests(APITestCase):
         url = "/api/v1/single_cells/?dataset=species1-dataset1"
         response = self.client.get(url, format="json")
         single_cells = response.data["results"]
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(single_cells), 1)
-        self.assertListEqual([s["name"] for s in single_cells], ["singleCell"])
+        assert response.status_code == status.HTTP_200_OK
+        assert len(single_cells) == 1
+        assert [s["name"] for s in single_cells] == ["singleCell"]
 
 
 class MetacellTests(APITestCase):
@@ -197,44 +197,44 @@ class MetacellTests(APITestCase):
         url = "/api/v1/metacells/?dataset=species3-dataset3"
         response = self.client.get(url, format="json")
         metacells = response.data["results"]
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(metacells), 2)
-        self.assertSetEqual({s["name"] for s in metacells}, {"meta1", "meta2"})
+        assert response.status_code == status.HTTP_200_OK
+        assert len(metacells) == 2
+        assert {s["name"] for s in metacells} == {"meta1", "meta2"}
 
     def test_retrieve_links(self):
         url = "/api/v1/metacell_links/?dataset=species3-dataset3"
         response = self.client.get(url, format="json")
         metacell_links = response.data["results"]
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(metacell_links), 1)
-        self.assertEqual(metacell_links[0]["metacell"], "meta1")
-        self.assertEqual(metacell_links[0]["metacell2"], "meta2")
+        assert response.status_code == status.HTTP_200_OK
+        assert len(metacell_links) == 1
+        assert metacell_links[0]["metacell"] == "meta1"
+        assert metacell_links[0]["metacell2"] == "meta2"
 
     def test_retrieve_gene_expression(self):
         url = "/api/v1/metacell_expression/?dataset=species3-dataset3"
         response = self.client.get(url, format="json")
         metacell_gene_expression = response.data["results"]
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(metacell_gene_expression), 4)
-        self.assertSetEqual({s["gene_name"] for s in metacell_gene_expression}, {"gene1", "gene2"})
-        self.assertSetEqual({s["metacell_name"] for s in metacell_gene_expression}, {"meta1", "meta2"})
+        assert response.status_code == status.HTTP_200_OK
+        assert len(metacell_gene_expression) == 4
+        assert {s["gene_name"] for s in metacell_gene_expression} == {"gene1", "gene2"}
+        assert {s["metacell_name"] for s in metacell_gene_expression} == {"meta1", "meta2"}
 
     def test_retrieve_gene_expression_single_gene(self):
         url = "/api/v1/metacell_expression/?dataset=species3-dataset3&genes=gene2"
         response = self.client.get(url, format="json")
         metacell_gene_expression = response.data["results"]
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(metacell_gene_expression), 2)
-        self.assertSetEqual({s["gene_name"] for s in metacell_gene_expression}, {"gene2"})
-        self.assertSetEqual({s["metacell_name"] for s in metacell_gene_expression}, {"meta1", "meta2"})
+        assert response.status_code == status.HTTP_200_OK
+        assert len(metacell_gene_expression) == 2
+        assert {s["gene_name"] for s in metacell_gene_expression} == {"gene2"}
+        assert {s["metacell_name"] for s in metacell_gene_expression} == {"meta1", "meta2"}
 
     def test_retrieve_cell_markers(self):
         url = "/api/v1/markers/?dataset=species3-dataset3&metacells=meta1&fc_min_type=mean"
         response = self.client.get(url, format="json")
         markers = response.data["results"]
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(markers), 2)
-        self.assertEqual(markers[0]["name"], "gene1")
+        assert response.status_code == status.HTTP_200_OK
+        assert len(markers) == 2
+        assert markers[0]["name"] == "gene1"
 
 
 class GeneListTests(APITestCase):
@@ -253,9 +253,9 @@ class GeneListTests(APITestCase):
         response = self.client.get(url, format="json")
         genelists = response.data["results"]
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(genelists), 2)
-        self.assertSetEqual({s["name"] for s in genelists}, {"geneList1", "geneList2"})
+        assert response.status_code == status.HTTP_200_OK
+        assert len(genelists) == 2
+        assert {s["name"] for s in genelists} == {"geneList1", "geneList2"}
 
 
 class DomainsTest(APITestCase):
@@ -274,9 +274,9 @@ class DomainsTest(APITestCase):
         url = "/api/v1/domains/"
         response = self.client.get(url, format="json")
         domains = response.data["results"]
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(domains), 2)
-        self.assertSetEqual({s["name"] for s in domains}, {"Domain1", "Domain2"})
+        assert response.status_code == status.HTTP_200_OK
+        assert len(domains) == 2
+        assert {s["name"] for s in domains} == {"Domain1", "Domain2"}
 
 
 class CorrelatedGenesTest(APITestCase):
@@ -298,11 +298,11 @@ class CorrelatedGenesTest(APITestCase):
         url = "/api/v1/correlated/?dataset=species1-dataset1&gene=gene1"
         response = self.client.get(url, format="json")
         correlations = response.data["results"]
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(correlations), 3)
-        self.assertSetEqual({s["gene"] for s in correlations}, {"gene3", "gene2", "gene4"})
-        self.assertSetEqual({s["spearman"] for s in correlations}, {0.5, 0.4, 0.56})
-        self.assertSetEqual({s["pearson"] for s in correlations}, {0.8, 0.7, 0.6})
+        assert response.status_code == status.HTTP_200_OK
+        assert len(correlations) == 3
+        assert {s["gene"] for s in correlations} == {"gene3", "gene2", "gene4"}
+        assert {s["spearman"] for s in correlations} == {0.5, 0.4, 0.56}
+        assert {s["pearson"] for s in correlations} == {0.8, 0.7, 0.6}
 
 
 class OrthologsTests(APITestCase):
@@ -326,18 +326,18 @@ class OrthologsTests(APITestCase):
         url = "/api/v1/orthologs/"
         response = self.client.get(url, format="json")
         orthologs = response.data["results"]
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(orthologs), 4)
-        self.assertSetEqual({s["gene_name"] for s in orthologs}, {"gene1", "gene2", "gene3", "gene4"})
+        assert response.status_code == status.HTTP_200_OK
+        assert len(orthologs) == 4
+        assert {s["gene_name"] for s in orthologs} == {"gene1", "gene2", "gene3", "gene4"}
 
     def test_counts(self):
         url = "/api/v1/ortholog_counts/"
         response = self.client.get(url, format="json")
         ortholog_counts = response.data["results"]
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(ortholog_counts), 1)
-        self.assertEqual(ortholog_counts[0]["species"], "species1")
-        self.assertEqual(ortholog_counts[0]["gene_count"], 4)
+        assert response.status_code == status.HTTP_200_OK
+        assert len(ortholog_counts) == 1
+        assert ortholog_counts[0]["species"] == "species1"
+        assert ortholog_counts[0]["gene_count"] == 4
 
 
 class SAMapTests(APITestCase):
@@ -360,11 +360,11 @@ class SAMapTests(APITestCase):
         url = "/api/v1/samap/?dataset=species3-dataset3&dataset2=species4-dataset4"
         response = self.client.get(url, format="json")
         samaps = response.data["results"]
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(samaps), 2)
-        self.assertSetEqual({s["metacell_type"] for s in samaps}, {"type1", "type2"})
-        self.assertSetEqual({s["metacell2_type"] for s in samaps}, {"type3", "type4"})
-        self.assertSetEqual({s["samap"] for s in samaps}, {0.8, 0.7})
+        assert response.status_code == status.HTTP_200_OK
+        assert len(samaps) == 2
+        assert {s["metacell_type"] for s in samaps} == {"type1", "type2"}
+        assert {s["metacell2_type"] for s in samaps} == {"type3", "type4"}
+        assert {s["samap"] for s in samaps} == {0.8, 0.7}
 
 
 @override_settings(MEDIA_ROOT=tempfile.mkdtemp())
@@ -382,20 +382,20 @@ class AlignTests(APITestCase):
             SpeciesFile.objects.get_or_create(species=species1, type="DIAMOND", defaults={"file": django_file})
 
     def check_expected_alignment(self, response):
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]["query"], "query")
-        self.assertEqual(response.data[0]["target"], "P0")
-        self.assertEqual(response.data[0]["identity"], "100")
-        self.assertEqual(response.data[0]["length"], "26")
-        self.assertEqual(response.data[0]["mismatch"], "0")
-        self.assertEqual(response.data[0]["gaps"], "0")
-        self.assertEqual(response.data[0]["query_start"], "1")
-        self.assertEqual(response.data[0]["query_end"], "26")
-        self.assertEqual(response.data[0]["target_start"], "1")
-        self.assertEqual(response.data[0]["target_end"], "26")
-        self.assertEqual(response.data[0]["e_value"], "4.41e-17")
-        self.assertEqual(response.data[0]["bit_score"], "51.6")
+        assert response.status_code == status.HTTP_200_OK
+        assert len(response.data) == 1
+        assert response.data[0]["query"] == "query"
+        assert response.data[0]["target"] == "P0"
+        assert response.data[0]["identity"] == "100"
+        assert response.data[0]["length"] == "26"
+        assert response.data[0]["mismatch"] == "0"
+        assert response.data[0]["gaps"] == "0"
+        assert response.data[0]["query_start"] == "1"
+        assert response.data[0]["query_end"] == "26"
+        assert response.data[0]["target_start"] == "1"
+        assert response.data[0]["target_end"] == "26"
+        assert response.data[0]["e_value"] == "4.41e-17"
+        assert response.data[0]["bit_score"] == "51.6"
 
     def test_get(self):
         url = "/api/v1/align/"
