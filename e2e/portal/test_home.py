29e4f5dc2c36ee97cb86eae2c7eafb2942f5806b
Andrés Becerra <andres.becerra@crg.eu>
2026-06-22 15:12:28 +0200

    Add test database for e2e execution

--- e2e/portal/test_home.py
+++ e2e/portal/test_home.py
@@ -12,3 +12,19 @@ class TestHomepage:
         expect(page.locator("#navbarSupportedContent").get_by_role("link", name="Docs")).to_be_visible()
         expect(page.locator("#navbarSupportedContent").get_by_role("link", name="Blog")).to_be_visible()
         expect(page.locator("#navbarSupportedContent").get_by_role("link", name="About")).to_be_visible()
+        expect(page.get_by_label("Tree of life.").get_by_role("img")).to_be_visible()
+
+    def test_species_datasets_links(self, page, live_server_url):
+        page.goto(self.base_url)
+        expect(page.get_by_role("link", name="species")).to_contain_text("2")
+        expect(page.get_by_role("link", name="species")).to_contain_text("species")
+        expect(page.get_by_role("link", name="datasets")).to_contain_text("2")
+        expect(page.get_by_role("link", name="datasets")).to_contain_text("datasets")
+
+    def test_dataset_select_opens(self, page, live_server_url):
+        page.goto(self.base_url)
+        page.get_by_role("combobox", name="Search datasets by species,").click()
+        expect(page.get_by_text("Porifera")).to_be_visible()
+        expect(page.get_by_text("Chordata")).to_be_visible()
+        expect(page.get_by_role("option", name="Amphineuron queenslandicum sponge")).to_be_visible()
+        expect(page.get_by_role("option", name="Homo sapiens (Baby) human")).to_be_visible()
