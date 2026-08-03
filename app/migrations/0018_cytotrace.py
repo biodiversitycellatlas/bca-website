fea99a62bac8a5c1e91c4c0ed3789fd41171709f
Nuno Agostinho <nuno.agostinho@crg.eu>
2026-08-03 16:59:24 +0200

    Fix migration conflict

--- app/migrations/0018_cytotrace.py
+++ app/migrations/0018_cytotrace.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('app', '0016_metacelltypesimilarity'),
+        ('app', '0017_expression_conservation'),
     ]
 
     operations = [
