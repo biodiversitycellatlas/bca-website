2955bae2d54cdc618874511f8962d22ff7a2f05a
Nuno Agostinho <nuno.agostinho@crg.eu>
2026-08-19 09:50:10 +0200

    Fix migration conflict

--- app/migrations/0019_tree_of_life.py
+++ app/migrations/0019_tree_of_life.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('app', '0014_add_mge_covering_index'),
+        ('app', '0018_cytotrace'),
     ]
 
     operations = [
