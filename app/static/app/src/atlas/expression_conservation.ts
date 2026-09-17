c32bd214222c13a832f2f2f0a22c7a683db8bf85
Nuno Agostinho <nuno.agostinho@crg.eu>
2026-09-17 16:37:14 +0200

    Lint code
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/static/app/src/atlas/expression_conservation.ts
+++ app/static/app/src/atlas/expression_conservation.ts
@@ -109,7 +109,8 @@ function loadOrthologExpression(id, dataset, gene, row) {
 
     // Update header above plot
     const linkGene = makeLinkGene()(row.gene, "display", row);
-    document.getElementById(`${id}_heading`).innerHTML = `${row.dataset_link} • ${linkGene}`;
+    document.getElementById(`${id}_heading`).innerHTML =
+        `${row.dataset_link} • ${linkGene}`;
 
     fetch(apiURL)
         .then((response) => response.json())
